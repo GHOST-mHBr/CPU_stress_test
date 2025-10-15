22d6960d64442f817b18641601a779f4912a878d
GHOST_mHBr <ghostmhbr@gmail.com>
2025-10-15 23:07:26 +0330

    Adding support for harwdare threads

--- main.cpp
+++ main.cpp
@@ -1,45 +1,44 @@
+#include <array>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
+#include <future>
 #include <iostream>
 #include <mutex>
+#include <random>
 #include <stdexcept>
 #include <string>
 #include <thread>
 #include <unistd.h>
 #include <vector>
-#include <array>
-#include <future>
-#include <random>
-#include <cstdlib>
 
 using time_point = std::chrono::time_point<std::chrono::steady_clock>;
 
-int main(int argc, const char **argv)
-{
+int main(int argc, const char **argv) {
   long number_of_threads{1};
   long number_of_steps{1};
 
-  if (argc != 3)
-  {
-    std::cout << "Usage: stress T S\nWhere T and S are unsigned integers representing number of threads and number of steps"
+  if (argc != 3) {
+    std::cout << "Usage: stress T S\nWhere T and S are unsigned integers "
+                 "representing number of threads and number of steps"
               << std::endl;
-    std::cout << "Bad usage. Give me the number of threads to run and the "
-                 "number of steps for each thread to run."
+    std::cout << "\nBad usage.\nYou MUST specify T and S as follows:\nT: Number "
+                 "of threads to run or -1 for number of CPU threads\nS: Number "
+                 "of steps to square/10e6"
               << std::endl;
     std::exit(1);
   }
 
-  try
-  {
+  try {
     number_of_threads = std::stol(argv[1]);
     number_of_steps = std::stol(argv[2]);
-    if(number_of_threads <= 0 || number_of_steps <= 0)
+    if (number_of_threads == -1) {
+      number_of_threads = std::thread::hardware_concurrency();
+    }
+    if (number_of_threads <= 0 || number_of_steps <= 0)
       throw std::invalid_argument{""};
-  }
-  catch (std::exception e)
-  {
+  } catch (std::exception e) {
     std::cout << "Bad number of threads or steps" << std::endl;
     std::exit(2);
   }
@@ -48,23 +47,20 @@ int main(int argc, const char **argv)
   time_point start_time = std::chrono::steady_clock::now();
 
   std::vector<std::future<void>> futures;
-  for (int i = 0; i < number_of_threads; i++)
-  {
-    futures.emplace_back(std::async(std::launch::async, [&]()
-                                    {
+  for (int i = 0; i < number_of_threads; i++) {
+    futures.emplace_back(std::async(std::launch::async, [&]() {
       double result = 1.0;
       for (uint32_t i = 0; i < number_of_steps; ++i) {
-        for (uint32_t j = 0; j < 1000000; j++) 
-        {
+        for (uint32_t j = 0; j < 1000000; j++) {
           result = std::sqrt(result + i);
           result = std::sin(result);
           result = std::cos(result);
         }
-      } }));
+      }
+    }));
   }
 
-  for (const std::future<void> &aFuture : futures)
-  {
+  for (const std::future<void> &aFuture : futures) {
     aFuture.wait();
   }
 
